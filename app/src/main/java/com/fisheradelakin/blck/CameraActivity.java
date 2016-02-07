6bfa60a240087361f5ebe4468ce7fd544440c138
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-02-07 02:22:44 -0500

    get started on camera screen

--- app/src/main/java/com/fisheradelakin/blck/CameraActivity.java
+++ app/src/main/java/com/fisheradelakin/blck/CameraActivity.java
@@ -1,13 +1,38 @@
 package com.fisheradelakin.blck;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.View;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity
+        implements View.OnClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     * Conversion from screen rotation to JPEG orientation.
+     */
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final String FRAGMENT_DIALOG = "dialog";
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
