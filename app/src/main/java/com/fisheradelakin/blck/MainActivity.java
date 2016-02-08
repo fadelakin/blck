e2b657d64c0e141b7e8d0526262ca691102030e4
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-02-07 19:16:57 -0500

    minor cleanup

--- app/src/main/java/com/fisheradelakin/blck/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/blck/MainActivity.java
@@ -3,13 +3,6 @@ package com.fisheradelakin.blck;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
@@ -20,21 +13,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.commonsware.cwac.cam2.CameraActivity;
+import android.view.View;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_PORTRAIT_BFC = 1;
     private static final String TAG = "MainActivity";
-    private File mFolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        mFolder = new File(Environment.getExternalStorageDirectory() + "/Blck");
+        File folder = new File(Environment.getExternalStorageDirectory() + "/Blck");
 
-        if (!mFolder.exists()) {
-            mFolder.mkdir();
+        if (!folder.exists()) {
+            folder.mkdir();
             Log.i(TAG, "Folder doesn't exist");
         } else {
             Log.i(TAG, "Folder exists");
@@ -72,54 +59,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
                 Intent intent = new Intent(MainActivity.this, com.fisheradelakin.blck.CameraActivity.class);
                 startActivity(intent);
-                /*Intent i = new CameraActivity.IntentBuilder(MainActivity.this)
-                        .skipConfirm()
-                        .to(new File(mFolder, "blck" + new Date().toString() + ".jpg"))
-                        .debug()
-                        .build();
-                startActivityForResult(i, REQUEST_PORTRAIT_BFC);*/
             }
         });
     }
-
-    /*@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_PORTRAIT_BFC && resultCode == RESULT_OK) {
-            Log.i(TAG, "Everything went well.");
-
-            if(data != null) {
-                Uri selectedImage = data.getData();
-                String fileString = selectedImage.getPath();
-                Bitmap bitmap = BitmapFactory.decodeFile(fileString, new BitmapFactory.Options());
-                Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-                //Bitmap bmpMonochrome = Bitmap.createBitmap(bitmap.getWidth(), bit, Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(mutableBitmap);
-                ColorMatrix ma = new ColorMatrix();
-                ma.setSaturation(0);
-                Paint paint = new Paint();
-                paint.setColorFilter(new ColorMatrixColorFilter(ma));
-                canvas.drawBitmap(mutableBitmap, 0, 0, paint);
-
-                FileOutputStream out = null;
-                try {
-                    out = new FileOutputStream(new File(mFolder, "blck" + new Date().toString() + ".jpg"));
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out); // bmp is your Bitmap instance
-                    // PNG is a lossless format, the compression factor (100) is ignored
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (out != null) {
-                            out.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }*/
 
     private void requestStoragePermission() {
         if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
