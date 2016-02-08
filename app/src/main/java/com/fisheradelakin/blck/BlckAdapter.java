7520b56c6b35f879eb3243f18e888e503e340ad7
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-02-08 11:33:18 -0500

    share intent

--- app/src/main/java/com/fisheradelakin/blck/BlckAdapter.java
+++ app/src/main/java/com/fisheradelakin/blck/BlckAdapter.java
@@ -1,6 +1,7 @@
 package com.fisheradelakin.blck;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -46,7 +47,8 @@ public class BlckAdapter extends RecyclerView.Adapter<BlckAdapter.ViewHolder> {
         return mFileList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener {
 
         SimpleDraweeView picture;
 
@@ -54,6 +56,22 @@ public class BlckAdapter extends RecyclerView.Adapter<BlckAdapter.ViewHolder> {
             super(itemView);
 
             picture = (SimpleDraweeView) itemView.findViewById(R.id.image);
+
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("image/jpeg");
+            share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(mFileList.get(getLayoutPosition())));
+            mContext.startActivity(Intent.createChooser(share, "Share Image"));
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            return true;
         }
     }
 }
