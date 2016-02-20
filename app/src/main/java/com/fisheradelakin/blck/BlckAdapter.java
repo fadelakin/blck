c22b587a8b32c4aabd58fd48c30304588fbe7eaf
Fisher Adelakin <temidayoadelakin@gmail.com>
2016-02-20 05:21:39 -0600

    blck 1.0

--- app/src/main/java/com/fisheradelakin/blck/BlckAdapter.java
+++ app/src/main/java/com/fisheradelakin/blck/BlckAdapter.java
@@ -8,7 +8,6 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
